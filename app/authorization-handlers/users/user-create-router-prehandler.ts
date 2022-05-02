e435d2f85bb0fb50340dc6e4d09bd30ff8d467eb
GnanaBillianD <gnanabillian.david@gmail.com>
2022-05-02 14:34:26 +0530

    test commit

--- app/authorization-handlers/users/user-create-router-prehandler.ts
+++ app/authorization-handlers/users/user-create-router-prehandler.ts
@@ -4,10 +4,6 @@ const canCreate = async (req, reply) => {
       .code(403)
       .send({ errors: ['You are not allowed to perform this action'] });
   }
-};
-
-// for (let i = 0; i < 12; i++) {
-//   console.log(i);
-// }
+}
 
 export default canCreate;
